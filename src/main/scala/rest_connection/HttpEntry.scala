70b2b0eb1760396d845ef3afd8e6eb6c449269f4
437580 <yannick.gladow@holidaycheck.com>
2016-07-07 19:37:09 +0200

    changed logging

--- src/main/scala/rest_connection/HttpEntry.scala
+++ src/main/scala/rest_connection/HttpEntry.scala
@@ -43,8 +43,8 @@ trait Service extends Protocols with HttpRequester {
 
         implicit val timeout = Timeout(5.seconds)
 
-        logger.debug(s"got single classify request for ${request.algorithm}")
-        logger.debug(s"with raw text: ${request.text}")
+        logger.info(s"got single classify request for ${request.algorithm}")
+        logger.info(s"with raw text: ${request.text}")
         val rawText = RawText(request.text)
         val req = RequestBuilding.Post("/clean", entity = HttpEntity(ContentTypes.`application/json`, rawText.toJson.compactPrint))
         val futureCleaningRes = futureHttpResponse(req ,settings.cleaning.host, settings.cleaning.port)
@@ -59,7 +59,7 @@ trait Service extends Protocols with HttpRequester {
         complete {
           classifyResult.map[ToResponseMarshallable] {
             case ClassificationResult(rep, dem) =>
-              logger.debug(s"classified with ${request.algorithm}, " + "rep:" + rep + ", dem:" + dem)
+              logger.info(s"classified with ${request.algorithm}, " + "rep:" + rep + ", dem:" + dem)
               ClassifyResult(request.algorithm, rep, dem).toJson
           }
         }
@@ -71,7 +71,7 @@ trait Service extends Protocols with HttpRequester {
 
       implicit val timeout = Timeout(5.seconds)
 
-      logger.debug(s"got twitter classify request for ${request.texts.size} posts.")
+      logger.info(s"got twitter classify request for ${request.texts.size} posts.")
       val bulkRaw = BulkRaw(request.texts)
       val req = RequestBuilding.Post("/cleanBulk", entity = HttpEntity(ContentTypes.`application/json`, bulkRaw.toJson.compactPrint))
       val futureCleaningRes = futureHttpResponse(req ,settings.cleaning.host, settings.cleaning.port)
@@ -88,7 +88,7 @@ trait Service extends Protocols with HttpRequester {
 
       val futureBulkRes = futureClassifyResults.map(res => BulkResult(res.map(cRes => ClassifyResult(request.algorithm, cRes.repProb, cRes.demProb))))
       futureBulkRes.onSuccess{
-        case res => logger.debug(s"classified twitter account with:  ${res.results.mkString(" ")} ")
+        case res => logger.info(s"classified twitter account with:  ${res.results.mkString(" ")} ")
       }
 
       complete(futureBulkRes)
